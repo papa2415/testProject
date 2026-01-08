ced484a344ef44b96e2f1c622e43d7bc8797c1eb
papa2415 <167390811+papa2415@users.noreply.github.com>
2026-01-08 16:40:18 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+import axios from "axios";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
